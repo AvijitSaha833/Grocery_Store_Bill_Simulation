aca9d4daffa9ca8568aca3dcebec3fdb25835f20
Avi <Avi@Gaming-Machine>
2021-05-05 14:05:58 +0530

    uploading project

---
+++ src/goods/Item.java
@@ -0,0 +1,34 @@
+package goods;
+
+public class Item {
+
+    private String category;
+    private String name;
+    private double price;
+    
+    public Item(String category, String name, double price) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price, String personal) {
+        this.price = price;
+    }
+
+    public void displayInfo(){
+    }
+    
+}//end class
